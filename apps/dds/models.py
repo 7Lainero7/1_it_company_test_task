71a5785e98449f7cbdb6ff350fe1f11ea4680686
Lainero <7Lainero7@gmail.com>
2025-07-28 18:35:40 +0500

    added models BaseModel, Status, TransactionType, Category, Subcategory

--- apps/dds/models.py
+++ apps/dds/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    id = models.AutoField(max_length=127, primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Status(BaseModel):
+    name = models.CharField(max_length=63, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class TransactionType(BaseModel):
+    name = models.CharField(max_length=63, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=127, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class SubCategory(BaseModel):
+    name = models.CharField(max_length=127)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subcategories')
+
+    class Meta:
+        unique_together = ('name', 'category')
+
+    def __str__(self):
+        return f"{self.category.name} → {self.name}"
