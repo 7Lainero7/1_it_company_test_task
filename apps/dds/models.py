7bc970519bd981a9df6fdec3624887035d68d856
Lainero <7Lainero7@gmail.com>
2025-07-28 18:37:29 +0500

    added model cashFlowRecord

--- apps/dds/models.py
+++ apps/dds/models.py
@@ -40,3 +40,16 @@ class SubCategory(BaseModel):
 
     def __str__(self):
         return f"{self.category.name} → {self.name}"
+
+
+class CashFlowRecord(BaseModel):
+    status = models.ForeignKey(Status, on_delete=models.PROTECT)
+    type = models.ForeignKey(TransactionType, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.PROTECT)
+    amount = models.DecimalField(max_digits=15, decimal_places=2)
+    comment = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.custom_date or self.created_at} — {self.amount} ₽"
+
